e71a6472fcfb893276f07b641766d6b519db5665
ogarose <alexey.deshkevich@gmail.com>
2021-06-19 12:11:02 +0300

    add badges

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,14 +3,9 @@ plugins {
     id("net.researchgate.release") version "2.8.1"
 }
 
+// net.researchgate.release requires a build task for root project.
 tasks.register("build") {
     println("stub required for net.researchgate.release plugin")
 
     dependsOn("mainapp:build")
 }
-
-
-//tasks.release {
-//    buildTasks = ['buildWar']
-//}
-//}
